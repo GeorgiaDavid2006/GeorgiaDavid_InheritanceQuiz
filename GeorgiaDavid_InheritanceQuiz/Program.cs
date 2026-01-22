84d84e524eb6b3467a7a81c8115c77a34d7521f7
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2026-01-22 12:14:24 -0400

    Seventh Commit

--- GeorgiaDavid_InheritanceQuiz/Program.cs
+++ GeorgiaDavid_InheritanceQuiz/Program.cs
@@ -8,6 +8,41 @@ namespace GeorgiaDavid_InheritanceQuiz
 {
     internal class Program
     {
+        static string[] quizQuestions =
+        {
+            "Question 1",
+            "Question 2",
+            "Question 3",
+            "Question 4",
+            "Question 5",
+            "Question 6",
+            "Question 7",
+            "Question 8",
+            "Question 9"
+        };
+
+        static string[] answers =
+        {
+            "Text Answer 1",
+            "Text Answer 2",
+            "Text Answer 3",
+            "Multiple Choice Answer 1",
+            "Multiple Choice Answer 2",
+            "Multiple Choice Answer 3",
+            "Multiple Choice Answer 4",
+            "Multiple Choice Answer 5",
+            "Multiple Choice Answer 6",
+            "Multiple Choice Answer 7",
+            "Multiple Choice Answer 8",
+            "Multiple Choice Answer 9",
+            "Multiple Choice Answer 10",
+            "Multiple Choice Answer 11",
+            "Multiple Choice Answer 12",
+            "True or False Answer 1",
+            "True or False Answer 2",
+            "True or False Answer 3"
+        };
+
         static void Main(string[] args)
         {
 
